8759d3ce235a73f8dcf7dc7e03a64248461f3ab7
Silvyjson <139075245+Silvyjson@users.noreply.github.com>
2024-01-31 08:10:21 +0100

    feat: created a header section

--- src/js-component/FooterSection.jsx
+++ src/js-component/FooterSection.jsx
@@ -3,23 +3,25 @@ import { Button, Inputs } from "./form";
 
 function Footer() {
     return (
-        <footer className="footer">
-            <span>
+        <footer className="footer_section">
+            <span className="newletter-section">
                 <h1>Newsletter</h1>
                 <p>Subscribe to our newsletter to get your weekly dose of news, updates, tips and special offers</p>
-                <div>
-                    <Inputs placeholder="Enter your email address" />
-                    <img src="./image/mail-02.png" alt="mail-logo" />
-                </div>
-                <Button label="Subscribe" />
+                <form action="" className="nesletter-form">
+                    <div>
+                        <Inputs placeholder="Enter your email address" />
+                        <img src="./image/mail-02.png" alt="mail-logo"  className="mail-logo"/>
+                    </div>
+                    <Button label="Subscribe" />
+                </form>
             </span>
-            <div className="footer-container">
+            {/* <div className="footer-container">
                 <div className="footer-logo">
                     <img src="./image/Finfuze logo 1 2.png" alt="logo" />
                 </div>
                 <div className="footer-links">
                 </div>
-            </div>
+            </div> */}
         </footer>
     );
 }
